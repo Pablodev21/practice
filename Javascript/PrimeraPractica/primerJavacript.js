336521c0a2e569cf5f3811889c675f2599ea3d60
Pablodev21 <108357129+Pablodev21@users.noreply.github.com>
2023-03-21 18:23:57 +0100

    primer commit

---
+++ Javascript/PrimeraPractica/primerJavacript.js
@@ -0,0 +1,18 @@
+
+/*
+Función básica para mostrar por consola 
+*/
+console.log("Esto es un ejemplo")
+
+
+/*
+const y let son variables genericas
+const indica que el valor no varia.
+let indica que el valor puede variar.
+  
+ */
+const num =1
+let num1 =1
+
+const palabras ="Hola bon dia"
+let [] 
