dedbb9d411340cd576b2668124179b107763dbdb
Pablodev21 <pabloprogramacion21@gmail.com>
2023-04-13 15:23:28 +0200

    actualizaciones

--- Javascript/CursoEdutin_Academy/calculadora/calculadora.js
+++ Javascript/CursoEdutin_Academy/calculadora/calculadora.js
@@ -1,12 +1,16 @@
-var digito1 = null;
+var digito1 = "";
 
-var digito2 =  null;
+var digito2 =  "";
 
 var signo_Operacion="";
 
-var texto_Calculadora ="";
+var texto_Calculadora = " ";
 
 var primeroTerminado = false;
+
+var segundoTerminado = false;
+
+
 
 
 
@@ -27,45 +31,89 @@ function recogerNumero(numeroInput){
     
 
     // let datos = document.getElementsByClassName(`${nombre_Clase}`).value;
-    var valor = document.getElementsByClassName('numero-Calculadora')[numeroInput].value;  
-    // if(digito1===null && digito2===null && signo_Operacion===""){
-
-    //     digito1+=valor;
-    //     // document.getElementsByClassName(nombre_Clase).innerHTML=digito1;
-
-    //     console.log(digito1+" ");
-
-    // }
-
-    // else if(digito1!=null && digito2===null ){
-
-    //     digito2=valor;
-    //     texto_Calculadora+=digito2; 
-    //     // document.getElementsByClassName('texto-calculadora').innerHTML=texto_Calculadora;
-
-    //     console.log(digito2);
-    // }
-
-
+    var valor = document.getElementsByClassName('numero-Calculadora')[numeroInput].value;
+    
+    
 
 
     if(primeroTerminado===false && signo_Operacion===""){
 
-        digito1+=
+        digito1+=valor;
+        texto_Calculadora+=digito1;
 
+        var pruebaTexto = document.getElementsByClassName('texto-Resultado');
+        
+        // Aqui viene pruebaTexto a null
+        pruebaTexto.innerHTML=texto_Calculadora;
+        pruebaTexto.value= texto_Calculadora;
+        console.log("hoa")
 
-    }else{
+        if(signo_Operacion!=""){
+
+            primeroTerminado=true;
+
+        } 
+
+        
 
 
     }
+    if(primeroTerminado===true){
+
+        digito2+=document.getElementsByClassName('numero-Calculadora')[numeroInput].value;
+    }
+
+    console.log("digito 1 " + digito1 + " digito 2 "+ digito2);
 
 }
 
-function recogerOperador(numeroIndex){
+
+    function realizarOperacion (){
+
+        comprobarIgual=true;
+
+        // Realizar operaciones //
+
+        if (signo_Operacion==="+") texto_Calculadora = digito1+digito2;
+
+        if(signo_Operacion==="-") texto_Calculadora = digito1-digito2;
+
+        if (signo_Operacion==="*") texto_Calculadora = digito1*digito2;
+
+        if(signo_Operacion==="/") texto_Calculadora = digito1/digito2;
+
+        else texto_Calculadora= "SYNTAX ERROR";
+
+    }
+
+
+    function reinicio(){
+
+        // Reinicio de variables //
+
+        digito1 = null;
+
+        digito2 =  null;
+
+        signo_Operacion="";
+
+        texto_Calculadora ="";
+
+        primeroTerminado = false;
+        
+        segundoTerminado = false;
+
+    }
+
+
+    function recogerOperador(numeroIndex){
 
         let datos = document.getElementsByClassName('operador-Calculadora')[numeroIndex].value;
 
         signo_Operacion = datos;
+
+    
+
         // texto_Calculadora+=signo_Operacion;
         // document.getElementsByClassName(texto).innerHTML=texto_Calculadora;
 
