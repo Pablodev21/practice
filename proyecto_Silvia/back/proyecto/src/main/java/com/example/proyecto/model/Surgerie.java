32cd5f311719c7d48bdf10be138f85e44eafc224
Pablodev21 <108357129+Pablodev21@users.noreply.github.com>
2023-04-18 12:09:31 +0200

    clases model necesarias por el momento

---
+++ proyecto_Silvia/back/proyecto/src/main/java/com/example/proyecto/model/Surgerie.java
@@ -0,0 +1,45 @@
+package com.example.proyecto.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Alumnos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Surgerie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotNull
+    @Column(name = "idClient")
+    private int idClient;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "date")
+    private Date date;
+
+
+
+    // Relación con clase cliente //
+    @ManyToOne
+    @JoinColumn(name = "idCLient", insertable = false, updatable = false)
+    private Client client;
+
+
+}
---
+++ proyecto_Silvia/back/proyecto/src/main/java/com/example/proyecto/model/User.java
@@ -0,0 +1,45 @@
+package com.example.proyecto.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotNull
+    @Column(name = "login")
+    private String login;
+
+    @NotNull
+    @Column (name="password")
+    private String password;
+
+    @NotNull
+    @Column(name = "rol")
+    private String rol;
+
+
+
+    public User(String login, String password, String rol) {
+        this.login = login;
+        this.password = password;
+        this.rol = rol;
+    }
+
+
+
+
+}
