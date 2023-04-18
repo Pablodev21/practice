c6be12e62d49c390423f4050fdf1ca2f7c1281be
Pablodev21 <108357129+Pablodev21@users.noreply.github.com>
2023-04-18 10:00:44 +0200

    archivos de config

---
+++ proyecto_Silvia/back/proyecto/src/main/java/com/example/proyecto/SilviaParadelaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilviaParadelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilviaParadelaApplication.class, args);
+	}
+
+}
