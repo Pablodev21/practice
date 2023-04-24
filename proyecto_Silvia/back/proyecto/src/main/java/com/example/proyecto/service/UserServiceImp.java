021851047db3c80a1893a3f49cddb8fa7ecf9949
Pablodev21 <108357129+Pablodev21@users.noreply.github.com>
2023-04-24 19:56:16 +0200

    solucion error clase userservice

--- proyecto_Silvia/back/proyecto/src/main/java/com/example/proyecto/service/UserServiceImp.java
+++ proyecto_Silvia/back/proyecto/src/main/java/com/example/proyecto/service/UserServiceImp.java
@@ -5,10 +5,12 @@ import com.example.proyecto.model.User;
 import com.example.proyecto.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImp implements UserService{
 
     @Autowired
