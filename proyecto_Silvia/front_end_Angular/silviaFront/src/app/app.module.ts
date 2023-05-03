6d03ea76b08266c14570a0070358ffddf2551fc0
Pablodev21 <108357129+Pablodev21@users.noreply.github.com>
2023-05-03 18:14:42 +0200

    cambops

--- proyecto_Silvia/front_end_Angular/silviaFront/src/app/app.module.ts
+++ proyecto_Silvia/front_end_Angular/silviaFront/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
